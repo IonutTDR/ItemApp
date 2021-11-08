e74fb51aa38861cb7ef118e987931a36c70ffd11
toaderionut <toader.ionut.serban@gmail.com>
2021-11-08 13:33:50 +0200

    Services

---
+++ Services/IItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public interface IItemRepository
+    {
+        void AddItem(Item item);
+        Item GetItem(int id);
+        IEnumerable<Item> GetItems();
+        bool ItemExists(int id);
+    }
+}
---
+++ Services/ItemRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccess.Contexts;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Services
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly ItemContext _ctx;
+
+        public ItemRepository(ItemContext ctx)
+        {
+            _ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+
+        public void AddItem(Item item)
+        {
+            _ctx.Items.Add(item);
+            _ctx.SaveChanges();
+        }
+
+        public Item GetItem(int id)
+        {
+            if (!ItemExists(id))
+                throw new Exception($"The item with this {id} does not exist!");
+            return _ctx.Items.FirstOrDefault(i => i.Id == id);
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            return _ctx.Items.Include(c => c.Comments).ToList();
+        }
+
+        public bool ItemExists(int id)
+        {
+            return _ctx.Items.Any(i => i.Id == id);
+        }
+    }
+}
